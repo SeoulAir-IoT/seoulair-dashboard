66bf6ea6689897ae2ba6fa5bebc2a3aeec8dad23
Uros Aleksandrovic <uros.aleksandrovic@primeholding.com>
2020-10-01 22:10:37 +0200

    Create design base for the aplciation.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,34 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatInputModule } from '@angular/material/input/';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle/'
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+
+import {StationInfoComponent} from './station-info/station-info.component';
+import { DeviceManagementComponent } from './device-management/device-management.component';
+import { NotificationsTableComponent } from './notifications-table/notifications-table.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeviceManagementComponent,
+    StationInfoComponent,
+    NotificationsTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
