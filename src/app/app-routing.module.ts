09629d50c5871c0e4301df74aac5ca60314a3cc3
Uros Aleksandrovic <uros.aleksandrovic@outlook.com>
2020-10-01 20:32:01 +0000

    Groundwork for angular application.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DeviceManagementComponent } from './device-management/device-management.component';
+import { StationInfoComponent } from './station-info/station-info.component';
+import { NotificationsTableComponent } from './notifications-table/notifications-table.component'
+
+const routes: Routes = [
+  { path: '', redirectTo: '/device', pathMatch: 'full' },
+  { path: 'device', component: DeviceManagementComponent },
+  { path: 'station-info', component: StationInfoComponent },
+  { path: 'notifications-table', component: NotificationsTableComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/station-info/station-info.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'station-info',
+    templateUrl: 'station-info.component.html',
+    styleUrls: ['station-info.component.scss']
+})
+export class StationInfoComponent {}
